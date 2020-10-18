16349b95060262e13b4b10b060c6d5626283f66a
RODRIGO ALFREDO DEL CARMEN HERNANDEZ <55514243+Assino666@users.noreply.github.com>
2020-10-18 18:53:37 -0500

    prefinalizado

--- Practicas/Practica 1/Problema 2/funcion.c
+++ Practicas/Practica 1/Problema 2/funcion.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+#include<stdlib.h>
+void intercambiarElementos(int* vector1, int tam,int i)
+{
+	int j,div;
+	int* vector2 = NULL;
+	vector2 = (int *)malloc(sizeof(int)*tam);
+		//ordenar datos
+	div=tam/2;
+	i=div;
+	for(j=0;j<div;j++){
+		vector2[j] = vector1[i];
+		i=i+1;
+	}
+		i=0;
+	for(j=div;j<tam;j++){
+		vector2[j]=vector1[i];
+		i=i+1;
+	}
+	for(i=0;i<tam;i++){
+		printf("vector en posicion %d su valor es %d\n",i,vector2[i]);
+	}
+	
+}
--- Practicas/Practica 1/Problema 2/main.c
+++ Practicas/Practica 1/Problema 2/main.c
@@ -1,41 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "Cabecera.h"
 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
 	//leer datos 
-	int tam,j,i,div;
+	int tam,i;
 	
 	printf("ingrese el tamaño del vector: ");
 	scanf("%d",&tam);
 	
 	int* vector1 = NULL;
-	int* vector2 = NULL;
+	
 	vector1 = (int *)malloc(sizeof(int)*tam);
-	vector2 = (int *)malloc(sizeof(int)*tam);
+	
 	for(i=0;i<tam;i++){
 		printf("ingrese el valor %d: ",i);
 		scanf("%d",&vector1[i]);
 		printf("\nvalor %d: \n",vector1[i]);
 	}
+	intercambiarElementos(vector1, tam, i);
 
-	//ordenar datos
-	div=tam/2;
-	i=div;
-	for(j=0;j<div;j++){
-		vector2[j]=vector1[i];
-		i=i+1;
-	}
-		i=0;
-	for(j=div;j<tam;j++){
-		vector2[j]=vector1[i];
-		i=i+1;
-	}
-	for(i=0;i<tam;i++){
-		printf("vector en posicion %d su valor es %d\n",i,vector2[i]);
-	}
+
 	
 	return 0;
 }
