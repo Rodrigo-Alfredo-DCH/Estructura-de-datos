e2905d3eb87a00b64535d103dd1b66275954b9c7
RODRIGO ALFREDO DEL CARMEN HERNANDEZ <55514243+Assino666@users.noreply.github.com>
2020-10-19 18:13:57 -0500

    definitiva

--- Practicas/Practica 1/Problema 5/main.c
+++ Practicas/Practica 1/Problema 5/main.c
@@ -3,27 +3,37 @@
 
 #include "matrices.h"
 
-void llenar_matriz(int, double **);
+//void llenar_matriz(int, double **);
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int orden;
+	int orden,i,j;
 	printf("dime el orden de la matriz: ");
-	scanf("%d, &orden");	
+	scanf("%d", &orden);	
 	double **matriz = crear_matriz(orden,orden);
-	llenar_matriz(orden, matriz);
-	printf("el determinante vale: %lf\n", calcularDeterminante(orden, matriz));
+	for(i=0; i<orden; i++) {
+      for(j=0; j<orden; j++){      
+         //scanf("%lf", mat[i][j]);
+         matriz[i][j] = rand();
+         printf("%lf	| ", matriz[i][j]);
+		 }
+		 printf("\n");
+	}
+	//llenar_matriz(orden, matriz);
+	printf("el determinante vale: %lf \n", calcularDeterminante(matriz, orden));
 	return 0;
 }
 
-void llenar_matriz(int orden, double **mat){
+/*void llenar_matriz(int orden, double **mat){
 	int i,j;
 	printf("\nIngrese los elementos de la matriz: \n");
-   for(i = 0; i<orden; i++) {
-      for(j = 0; j<orden; j++){      
-         scanf("%lf", mat[i][j]);
+   for(i=0; i<orden; i++) {
+      for(j=0; j<orden; j++){      
+         //scanf("%lf", mat[i][j]);
+         mat[i][j] = rand();
+         print("%lf",mat[i][j]);
 		 }
 	}
 	
-}
+}*/
