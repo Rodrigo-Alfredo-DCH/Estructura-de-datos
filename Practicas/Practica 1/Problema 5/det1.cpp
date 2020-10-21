2c69ae9ccd6948fb2956916bae4138a7ac2cb169
BryanGuzman99 <73077107+BryanGuzman99@users.noreply.github.com>
2020-10-20 22:33:14 -0500

    Rar

--- Practicas/Practica 1/Problema 5/det1.cpp
+++ Practicas/Practica 1/Problema 5/det1.cpp
@@ -1,24 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 int main(){
+	
 	int tam,i,j,k;
+	
 	printf("ingrese el tamaño: ");
 	scanf("%d",&tam);
+	
+	
 	//declarando matriz dinamica
+	
 	int** mat = NULL;
+	
 	mat = (int **)malloc( sizeof(int*) * tam);
+	
 	for(i=0;i<tam;i++){
 		mat[i] = (int*)malloc( sizeof(int)* tam);
 	}
 	
 	float aux, pivote,pivote1,det=1;
+	
 	printf("\n Llenando la matriz");
+	
 	for(i=0;i<tam;i++){
 		for(j=0;j<tam;j++){
 			mat[i][j]=rand();
 		}
 	}
+	
 	printf("\n Imprimiendo la matriz\n");
+	
 	for(i=0;i<tam;i++){
 		for(j=0;j<tam;j++){
 			printf("| %d	",mat[i][j]);
@@ -26,7 +38,9 @@ int main(){
 		printf("\n");
 	}
 	
+	
 	//REDUCCION POR RENGLONES
+	
 	for(i=0;i<tam;i++){
 		pivote=mat[i][i];
 		for(j=i+1; j<tam;j++){
@@ -37,13 +51,14 @@ int main(){
 			}
 		}
 	}
+	
+	
 	// CALCULO DE LA DETERMINANTE
+	
 	for(i=0;i<tam;i++)
 		det+=mat[i][i];
 	
-	
 	printf("\n la determinante es: %f",det);
-	
 	
 	return 0;
 	
